75a7321ac1f6a53cec3b6d9c6ad530128b26e359
datblag <datblag@gmail.com>
2020-03-19 14:30:59 +0900

    reformat

--- polik3bot_main.py
+++ polik3bot_main.py
@@ -1,3 +1,5 @@
+from os.path import curdir
+
 from telegram.ext import Updater, CommandHandler, MessageHandler
 from bot_config import TOKEN, REQUEST_KWARGS, QSYSTEM_ADDRESS, TELECRAM_CLIENT_WHITE_LIST, TELEGRAM_GLPI_LIST
 import socket
@@ -86,7 +88,7 @@ def handle_text(update, context):
 
                 match = next((l for l in TELEGRAM_GLPI_LIST if l['chat_id'] == cur_chat), None)
 
-                if not match is None:
+                if match is not None:
                     user_token = match['glpi_token']
 
                     target_url = 'initSession/'
@@ -96,7 +98,7 @@ def handle_text(update, context):
                     session_token = session.json()
                     session_token = session_token['session_token']
                     args = None
-                    item_type='ticket'
+                    item_type = 'ticket'
                     if args is None:
                         target_url = item_type + '/'
                     else:
@@ -107,24 +109,18 @@ def handle_text(update, context):
                     for ticket in session.json():
                         print(ticket)
 
-
-
                     target_url = item_type+'/'
                     sessiondata = {'Content-Type': 'application/json',
                                    'Session-Token': session_token, 'App-Token': app_token}
-                    session = post(base_url + target_url, headers=sessiondata, json={"input":{"name":name_ticket,'users_id_recipient':None,'content':name_ticket,'itilcategories_id':1}})
-                    print(session.json())
+                    session = post(base_url + target_url, headers=sessiondata, json={"input": {"name": name_ticket,
+                                   'users_id_recipient': None, 'content': name_ticket, 'itilcategories_id': 1}})
                     update.message.reply_text(session.json()['message'])
 
                     target_url = item_type+'/'+str(session.json()['id'])
                     print(target_url)
                     sessiondata = {'Content-Type': 'application/json',
                                    'Session-Token': session_token, 'App-Token': app_token}
-                    session = put(base_url + target_url, headers=sessiondata, json={"input":{'status':1}})
-                    print(session.json())
-
-                    #json.decoder()
-
+                    session = put(base_url + target_url, headers=sessiondata, json={"input": {'status': 1}})
 
                     target_url = 'killSession'
                     sessiondata = {'Content-Type': 'application/json',
@@ -136,10 +132,9 @@ def handle_text(update, context):
                 update.message.reply_text('ошибка не указан заголовок тикета')
 
 
-
 def help(update, context):
-    cur_chat=update['message']['chat']['id']
-    if check_telegram_client(update['message']['chat']['id'],update):
+    cur_chat = update['message']['chat']['id']
+    if check_telegram_client(cur_chat, update):
         update.message.reply_text('/tadd заголовок - добавить тикет')
 
 
@@ -150,14 +145,14 @@ def main():
     # data = sock.recv(1024)
     # data2 = json.loads(data.decode("cp1251"))
     # print(data2)
-    # sock.close()
+    # sock.close()01011960
 
-    # sock = socket.socket()
-    # sock.connect(QSYSTEM_ADDRESS)
-    # sock.send(b'{"params":{"drop_tickets_cnt":false},"jsonrpc":"2.0","id":"1570170084856","method":"Empty"}')
-    # data = sock.recv(1024)
-    # data2 = json.loads(data.decode("cp1251"))
-    # print(data2)
+    sock = socket.socket()
+    sock.connect(QSYSTEM_ADDRESS)
+    sock.send(b'{"params":{"drop_tickets_cnt":false},"jsonrpc":"2.0","id":"1570170084856","method":"Empty"}')
+    data = sock.recv(1024)
+    data2 = json.loads(data.decode("cp1251"))
+    print(data2)
 
     updater = Updater(TOKEN, use_context=True)
     # updater = Updater(TOKEN, use_context=True, request_kwargs=REQUEST_KWARGS)
